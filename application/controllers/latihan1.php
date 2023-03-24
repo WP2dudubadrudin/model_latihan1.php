cc15645741288497288cc795d28245257c6a78bf
dudu_b <dudubadrudin88@gmail.com>
2023-03-24 11:58:50 +0700

    first commit

--- application/controllers/latihan1.php
+++ application/controllers/latihan1.php
@@ -11,7 +11,7 @@ class latihan1 extends CI_Controller
         $this->load->model('Model_latihan1');
         $data['nilai1'] = $n1;
         $data['nilai2'] = $n2;
-        $data['hasil'] = $this->Model_latihan->jumlah($n1, $n2);
+        $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
 
         $this->load->view('view-latihan', $data);
  
